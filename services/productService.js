af46d23b507100f41c13ae947aabede8fbcb3523
yiyiay <yigit.dinc@ug.bilkent.edu.tr>
2024-09-29 14:40:07 +0300

    feat:

--- services/productService.js
+++ services/productService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3003';
+const API_URL = 'https://yigit1.marqby.us';
 
 export const getProducts = async () => {
   const response = await axios.get(`${API_URL}/products`);
